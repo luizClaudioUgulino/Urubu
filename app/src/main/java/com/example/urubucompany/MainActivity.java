c1dc2f8defdf2dc8488cdcee56d3d8249dac110b
Luiz Claudio Ugulino Rodrigues Da Cunha <lcunha@aviva.com.br>
2023-07-28 12:12:10 -0300

    estudo android studio 01

--- app/src/main/java/com/example/urubucompany/MainActivity.java
+++ app/src/main/java/com/example/urubucompany/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.urubucompany;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.filament.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void sortearNumero(android.view.View view) {
+        TextView texto = findViewById(R.id.textoResultado);
+        texto.setText("5");
     }
 }
